a63d761025aff00a715449e4233304c2f6c0b7fa
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2017-11-12 14:52:45 +0200

    Create a Network class

---
+++ src/network.js
@@ -0,0 +1,69 @@
+const { Neuron } = require('./neuron');
+
+class Network {
+    /**
+     * Constructs a neural network with Neurons
+     * @param {number} inputFeatures Number of input features, i.e. input vector size
+     * @param {number} hiddenLayers Number of hidden layers
+     * @param {number} outputClasses Number of output classes
+     * @param {function} nonlinearity The nonlinearity function to use in the Neurons
+     */
+    constructor(inputFeatures, hiddenLayers, outputClasses, nonLinearity) {
+        let initialWeights = new Array(inputFeatures).fill(1);
+        let initialBias = 0;
+        this.outputLayer = new Array(outputClasses)
+            .fill()
+            .map(item => new Neuron(initialWeights, initialBias, nonLinearity));
+
+        this.hiddenLayers = new Array(hiddenLayers)
+            .fill()
+            .map(layer =>
+                new Array(inputFeatures)
+                    .fill()
+                    .map(
+                        item =>
+                            new Neuron(
+                                initialWeights,
+                                initialBias,
+                                nonLinearity
+                            )
+                    )
+            );
+    }
+    /**
+     * Randomizes all the weights in the network
+     */
+    randomize() {
+        this.hiddenLayers.map(layer => layer.map(neuron => neuron.randomize()));
+        this.outputLayer.map(neuron => neuron.randomize());
+    }
+    /**
+     * Computes a full forward pass of the network
+     * @param {[number]} input Input vector, as a number array
+     * @returns {[number]} Final activations at the output layer
+     */
+    forwardPass(input) {
+        let hiddenOutput = this.hiddenLayers.reduce(
+            this.activateNeurons,
+            input
+        );
+        let finalOutput = this.activateNeurons(hiddenOutput, this.outputLayer);
+        return finalOutput;
+    }
+    backPropagate() {
+        // TODO
+        throw new Error('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * Helper function for neuron activations, used in forwardPass() as the reducer
+     * @param {[number]} currentInput Input vector we're currently interested in
+     * @param {[Neuron]} layer Layer of neurons we're running the activations of
+     */
+    activateNeurons(currentInput, layer) {
+        // For each layer, activate all neurons & return their values
+        return layer.map(neuron => neuron.activate(currentInput));
+    }
+}
+
+exports.Network = Network;
---
+++ test/network.js
@@ -0,0 +1,80 @@
+const assert = require('assert');
+const should = require('chai').should();
+const { Network } = require('../src/network');
+const { Neuron } = require('../src/neuron');
+
+class DummyNonLinearity {
+    // Dummy linear activation function
+    forward(value) {
+        return value;
+    }
+    backward(value) {
+        return 1;
+    }
+}
+
+describe('Network', () => {
+    let inputFeatures = 2;
+    let hiddenLayers = 2;
+    let outputClasses = 2;
+    let initialWeights = new Array(inputFeatures).fill(1);
+    let initialBias = 0;
+    let nonLinearity = new DummyNonLinearity();
+    let network = new Network(
+        inputFeatures,
+        hiddenLayers,
+        outputClasses,
+        nonLinearity
+    );
+    describe('Properties', () => {
+        it('Should be an object', () => {
+            network.should.be.an.instanceof(Object);
+        });
+        it('Should have hidden layers', () => {
+            network.hiddenLayers.should.be.an.instanceof(Array);
+        });
+        it('Should have an output layer', () => {
+            network.outputLayer.should.be.an.instanceof(Array);
+        });
+    });
+    describe('Output layer', () => {
+        it('Should have neurons', () => {
+            network.outputLayer.map(neuron =>
+                neuron.should.be.an.instanceof(Neuron)
+            );
+        });
+    });
+    describe('Hidden layers', () => {
+        it('Should exist', () => {
+            network.hiddenLayers.should.be.an.instanceof(Array);
+        });
+        it('Should have neurons', () => {
+            network.hiddenLayers.map(layer =>
+                layer.map(neuron => neuron.should.be.an.instanceof(Neuron))
+            );
+        });
+    });
+    describe('Randomize', () => {
+        it('Should be a function', () => {
+            network.randomize.should.be.an.instanceof(Function);
+        });
+        it('Should randomize the weights', () => {
+            let initialString = JSON.stringify(network);
+            network.randomize();
+            let afterString = JSON.stringify(network);
+            initialString.should.not.equal(afterString);
+        });
+    });
+    describe('Forward pass', () => {
+        it('Should be a function', () => {
+            network.forwardPass.should.be.an.instanceof(Function);
+        });
+        it('Should calculate the final activations', () => {
+            let testInput = [0, 1];
+            network.randomize();
+            let output = network.forwardPass(testInput);
+            console.log(output);
+            output.should.be.an.instanceof(Array);
+        });
+    });
+});
